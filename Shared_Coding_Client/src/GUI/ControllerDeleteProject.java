f3e6ffe52a9cb33a823ca3293f357034c1193a16
michaelm43 <45266524+michaelm43@users.noreply.github.com>
2020-06-16 00:54:31 +0300

    menu button work

---
+++ Shared_Coding_Client/src/GUI/ControllerDeleteProject.java
@@ -0,0 +1,83 @@
+package GUI;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import HttpRequests.UserRequests;
+import Logic.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ControllerDeleteProject {
+
+	private Stage deleteProjectStage;
+	private List<CheckBox> checkBoxFiles;
+	private User user;
+	private ControllerEditor controllerEditor;
+	
+	@FXML private VBox vboxCheckList;
+	@FXML private Button btnDelete;
+	@FXML private Button btnCancel;
+	
+	public ControllerDeleteProject(ControllerEditor controllerEditor, User user) {
+		this.deleteProjectStage = new Stage();
+		this.user = user;
+		this.controllerEditor = controllerEditor;
+		this.vboxCheckList = new VBox();
+		
+		this.checkBoxFiles= new ArrayList<>();
+		for(int i = 0; i < user.getProjectList().size(); i++) 
+			checkBoxFiles.add(new CheckBox(user.getProjectList().get(i)));
+
+		
+		// Load the FXML file
+        try {    		     	
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LayoutDeleteProject.fxml"));
+
+            // Set this class as the controller
+            loader.setController(this);
+
+            // Load the scene
+            deleteProjectStage.setScene(new Scene(loader.load()));
+
+            // Setup the window/stage
+            deleteProjectStage.setTitle("delete projects");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+		this.vboxCheckList.getChildren().addAll(checkBoxFiles);
+		deleteProjectStage.initModality(Modality.APPLICATION_MODAL);
+		deleteProjectStage.setResizable(false);
+	}
+	 
+	
+	public void showStage() {
+		this.deleteProjectStage.show();
+	}
+	
+	@FXML
+	public void deleteFiles() throws IOException {
+		if(ConfirmBox.display("WARNING", "are you sure you want to delete selected project?!?")) {
+			for(int i=0;i<checkBoxFiles.size();i++)
+				if(checkBoxFiles.get(i).isSelected())
+					this.user.removeProject(checkBoxFiles.get(i).getText());
+			new UserRequests().editUser(user);
+			this.controllerEditor.setUser(user);
+			this.deleteProjectStage.close();
+		}
+	}
+	
+	@FXML 
+	public void cancel() throws IOException {
+		this.deleteProjectStage.close();
+	}
+}
---
+++ Shared_Coding_Client/src/GUI/ControllerPassword.java
@@ -0,0 +1,87 @@
+package GUI;
+
+import java.io.IOException;
+
+import HttpRequests.UserRequests;
+import Logic.User;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ControllerPassword {
+	
+	private final Stage changePasswordStage;
+	private User user;
+	private ControllerEditor editor;
+	
+	@FXML private PasswordField txtOldPassword;
+	@FXML private PasswordField txtNewPassword;
+
+	@FXML private Label lblErrorMessage;
+	
+	public ControllerPassword(ControllerEditor editor ,User user) {
+		this.changePasswordStage = new Stage();
+		this.user = user;
+		this.editor = editor;
+		
+		// Load the FXML file
+        try {    		     	
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LayoutPassword.fxml"));
+
+            // Set this class as the controller
+            loader.setController(this);
+
+            // Load the scene
+            changePasswordStage.setScene(new Scene(loader.load()));
+
+            // Setup the window/stage
+            changePasswordStage.setTitle("change password");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+		changePasswordStage.initModality(Modality.APPLICATION_MODAL);
+		changePasswordStage.setResizable(false);
+	}
+	 
+	
+	public void showStage() {
+		this.changePasswordStage.show();
+	}
+
+	
+	@FXML
+	public void changePassword(){
+		//TODO update server
+		new ControllerRegister();
+
+		if(user.getPassword().equals(txtOldPassword.getText())) {
+			if(ControllerRegister.isValidPassword(txtNewPassword.getText()) &&
+					txtOldPassword.getText()!=txtOldPassword.getText()) {
+				user.setPassword(txtNewPassword.getText());
+				new UserRequests().editUser(user);
+				this.changePasswordStage.close();
+				editor.setUser(user);
+			}
+			else {
+				lblErrorMessage.setText("the new password is not valid");
+				lblErrorMessage.setVisible(true);
+			}
+		}
+		else {
+			lblErrorMessage.setText("the password is incorect");
+			lblErrorMessage.setVisible(true);
+		}
+	}
+	
+	@FXML
+	public void cancel() {
+		this.changePasswordStage.close();
+	}
+
+}
